4d7f648d1e4da4b8c94a86916ec49c48b3f4ec3c
Cizzy <callumanddev@gmail.com>
2024-09-18 16:26:47 +0100

    idrk

---
+++ UX creation/Assets/Code/Main buttons/ButtonsManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsManager : MonoBehaviour
+{
+    [Header("Screen gameobjects")]
+    public GameObject MainScreen;
+    public GameObject OptionsScreen;
+
+    public void Play(){
+        // Load level select screen
+        Debug.Log("Loaded level select");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Options(){
+        Debug.Log("Loaded options");
+        // Hide main menu
+        MainScreen.SetActive(false);
+
+        OptionsScreen.SetActive(true);
+    }
+
+    public void Quit(){
+        Application.Quit();
+        Debug.Log("Quit game");
+    }
+
+    public void Back(){
+        MainScreen.SetActive(true);
+        OptionsScreen.SetActive(false);
+    }
+
+    public void Backshots(){
+        SceneManager.LoadScene("Menu");
+    }
+}
